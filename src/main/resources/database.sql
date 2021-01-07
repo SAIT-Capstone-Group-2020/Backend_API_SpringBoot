f9999ea313b53005157aea2713e0ab0284fbd063
minhochoi <minhocalgary@gmail.com>
2021-01-06 17:33:39 -0700

    updated schema for proudct and sales item

--- src/main/resources/database.sql
+++ src/main/resources/database.sql
@@ -20,23 +20,21 @@
 --
 
 DROP SCHEMA IF EXISTS `hha` ;
-
 -- -----------------------------------------------------
 -- Schema hha
 -- -----------------------------------------------------
 CREATE SCHEMA IF NOT EXISTS `hha` DEFAULT CHARACTER SET latin1 ;
 USE `hha` ;
 
+-- 'category' table
+
 DROP TABLE IF EXISTS `category`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `category` (
-  `id` int(11) NOT NULL,
-  `name` varchar(45) NOT NULL,
-  `code` varchar(4) NOT NULL,
-  `description` longtext,
-  `image` varchar(45) NOT NULL,
-  PRIMARY KEY (`id`)
+                            `id` int(11) NOT NULL,
+                            `name` varchar(45) NOT NULL,
+                            PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -46,28 +44,31 @@ CREATE TABLE `category` (
 
 LOCK TABLES `category` WRITE;
 /*!40000 ALTER TABLE `category` DISABLE KEYS */;
-INSERT INTO `category` VALUES (1,'Frozen','FROZ','Product in Frozen','frozenDept.png'),(2,'Meat','MEAT','Product in Meat Department','meat.png');
+/*INSERT INTO `category` VALUES (1,'Frozen','FROZ','Product in Frozen','frozenDept.png'),(2,'Meat','MEAT','Product in Meat Department','meat.png');
 /*!40000 ALTER TABLE `category` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
--- Table structure for table `product`
+-- `product` table
 --
 
 DROP TABLE IF EXISTS `product`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `product` (
-  `id` int(11) NOT NULL,
-  `name` varchar(45) NOT NULL,
-  `code` varchar(4) NOT NULL,
-  `descrription` longtext,
-  `price` float NOT NULL,
-  `image` varchar(45) DEFAULT NULL,
-  `category` int(11) NOT NULL,
-  PRIMARY KEY (`id`),
-  KEY `fk_product_category_idx` (`category`),
-  CONSTRAINT `fk_product_category` FOREIGN KEY (`category`) REFERENCES `category` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+                           `id` int(11) NOT NULL,
+                           `name` varchar(45) NOT NULL,
+                           `description` longtext,
+                           `price` float NOT NULL,
+                           `active` BOOLEAN NOT NULL,
+                           `image` varchar(45) DEFAULT NULL,
+                           `category` int(11) NOT NULL,
+                           `inventory` int(11) NOT NULL,
+                           `last_updated` date not null,
+                           `expired_date` date,
+                           PRIMARY KEY (`id`),
+                           KEY `fk_product_category_idx` (`category`),
+                           CONSTRAINT `fk_product_category` FOREIGN KEY (`category`) REFERENCES `category` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -77,10 +78,73 @@ CREATE TABLE `product` (
 
 LOCK TABLES `product` WRITE;
 /*!40000 ALTER TABLE `product` DISABLE KEYS */;
-INSERT INTO `product` VALUES (1,'Frozen Crap','FCRA','Frozen Crap',10.99,'image.png',1),(2,'Frozen Crap','FCRA','Frozen Crap',10.99,'image.png',1),(3,'Beef finger meat','BEFM','Beef finger meat',10.99,'image.png',2);
+/*INSERT INTO `product` VALUES (1,'Frozen Crap','FCRA','Frozen Crap',10.99,'image.png',1),(2,'Frozen Crap','FCRA','Frozen Crap',10.99,'image.png',1),(3,'Beef finger meat','BEFM','Beef finger meat',10.99,'image.png',2);
 /*!40000 ALTER TABLE `product` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+--
+-- 'item' table
+-- salesPrice updated(triggered) on server side daily & when promotion table updated
+
+DROP TABLE IF EXISTS `sales_item`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `sales_item` (
+                              `id` int(11) NOT NULL,
+                              `product_id` int(11) NOT NULL,
+                              `sales_price` float not null,
+                              PRIMARY KEY (`id`),
+                              KEY `fk_item_productId_idx` (`product_id`),
+                              CONSTRAINT `fk_item_productId_idx` FOREIGN KEY (`product_id`) REFERENCES `product` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- 'promotion' table
+-- should decrease the limit when order was made
+
+DROP TABLE IF EXISTS `promotion`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `promotion` (
+                             `id` int(11) NOT NULL,
+                             `product_id` int(11) NOT NULL,
+                             `start_date` date not null,
+                             `end_date` date not null,
+                             `discount` float not null,
+                             `limit` int(11) not null,
+                             PRIMARY KEY (`id`),
+                             KEY `fk_promotion_productId_idx` (`product_id`),
+                             CONSTRAINT `fk_promotion_productId_idx` FOREIGN KEY (`product_id`) REFERENCES `product` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+
+--
+-- 'order' table
+--
+
+DROP TABLE IF EXISTS `order`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `order` (
+                         `id` int(11) NOT NULL,
+                         `order_date` date NOT NULL,
+                         `pickup_date` date NOT NULL,
+                         `status` varchar(10),
+                         `total` float not null,
+                         `payment_id` int(11) not null,
+                         `item_id` int(11) not null,
+                         PRIMARY KEY (`id`),
+                         KEY `fk_order_itemId_idx` (`item_id`),
+                         CONSTRAINT `fk_order_itemId_idx` FOREIGN KEY (`item_id`) REFERENCES `sales_item` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+    /*KEY `fk_order_paymentId_idx` (`payment_id`),
+    CONSTRAINT `fk_order_paymentId_idx` FOREIGN KEY (`payment_id`) REFERENCES `payment` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION*/
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
 /*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
@@ -91,3 +155,4 @@ UNLOCK TABLES;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
 -- Dump completed on 2020-12-27 19:55:11
+
